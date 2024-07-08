6eb0b31b6b9d42d489e9f83a6f83546cb8c600d0
SHUBHAM THAKUR <117472188+ShubhamThakur101@users.noreply.github.com>
2024-07-08 02:02:19 +0000

    Datatypes

---
+++ 03_Datatype/index.js
@@ -0,0 +1,32 @@
+"use strict";// treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3);
+console.log("shubh"); // code readability should be high
+
+// documentation -- https://tc39.es/ecma262/ and https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+
+
+let name = "shubham";
+let age = 21 ;
+let isLoggedIn = false;
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// Symbol => unique
+
+//object
+
+console.log(name,typeof(name))
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+// assignment-- read the mdn and tc39 documentation
