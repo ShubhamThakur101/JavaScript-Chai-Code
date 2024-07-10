0127b269f78308cb0a2bdc8f28d47b9e3bbaf371
SHUBHAM THAKUR <117472188+ShubhamThakur101@users.noreply.github.com>
2024-07-10 15:13:07 +0000

    StackandHeap

---
+++ 07_StackandHeap/index.js
@@ -0,0 +1,22 @@
+// Stack(Primitve), Heap(Non-Primitive)
+
+let myYoutubeName = "shubhvlog";
+
+let anotherName = myYoutubeName;
+
+anotherName = "rishuVlog";
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne = {
+    email : 'shubha@gmail.com',
+    upi : 'user@ybl'
+}
+
+let userTwo = userOne;
+
+userTwo.email = 'rishu@gmail.com';
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
